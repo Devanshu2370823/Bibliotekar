69d396612155c00045cb56270f1cac6795ea813b
2370823 <2370823@LTIN509410.cts.com>
2024-12-16 21:35:15 +0530

    Created project and Entity

---
+++ Bibliotekar/src/main/java/com/cts/bibliotekar/BibliotekarApplication.java
@@ -0,0 +1,13 @@
+package com.cts.bibliotekar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotekarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotekarApplication.class, args);
+	}
+
+}
---
+++ Bibliotekar/src/test/java/com/cts/bibliotekar/BibliotekarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.bibliotekar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotekarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
